d973961b8ebff31c934e5dc412c38d5a987e7a23
Darrick Fauvel <darrick.fauvel@gmail.com>
2021-12-03 14:51:27 -0500

    Build out Tours component

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
-};
+import React, { useEffect, useState } from 'react'
+import Tour from './Tour'
 
-export default Tours;
+const Tours = (props) => {
+  const { tours, removeTour } = props
+
+  return (
+    <>
+      <div className='title'>
+        <h2>our tours</h2>
+        <div className='underline'></div>
+      </div>
+      {tours.map((tour) => (
+        <Tour tour={tour} tours={tours} removeTour={removeTour} key={tour.id} />
+      ))}
+    </>
+  )
+}
+
+export default Tours
