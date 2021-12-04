ff81b60d2790f324135253339fe9a9a3e971a9ab
Darrick Fauvel <darrick.fauvel@gmail.com>
2021-12-03 19:35:08 -0500

    Remove state

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,25 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import data from './data'
 import SingleQuestion from './Question'
 
 function App() {
-  const [showAnswer, setShowAnswer] = useState(false)
-
-  const toggleButton = () => {
-    setShowAnswer(!showAnswer)
-  }
-
   return (
     <main>
       <div className='container'>
         <h3>questions and answers about login</h3>
         <section className='info'>
           {data.map((question) => (
-            <SingleQuestion
-              question={question}
-              toggleButton={toggleButton}
-              key={question.id}
-            />
+            <SingleQuestion question={question} key={question.id} />
           ))}
         </section>
       </div>
