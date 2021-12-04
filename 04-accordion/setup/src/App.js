60f8ae0a3a93967d392af90d23d2a6f0853d9c1c
Darrick Fauvel <darrick.fauvel@gmail.com>
2021-12-03 19:18:35 -0500

    Build App component

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,30 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from 'react'
+import data from './data'
+import SingleQuestion from './Question'
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [showAnswer, setShowAnswer] = useState(false)
+
+  const toggleButton = () => {
+    setShowAnswer(!showAnswer)
+  }
+
+  return (
+    <main>
+      <div class='container'>
+        <h3>questions and answers about login</h3>
+        <section class='info'>
+          {data.map((question) => (
+            <SingleQuestion
+              question={question}
+              toggleButton={toggleButton}
+              key={question.id}
+            />
+          ))}
+        </section>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
